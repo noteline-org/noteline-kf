8bde11aa584fa233a28e2dfef8cd6e82008b3e54
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2020-05-01 16:25:36 -0700

    packaging now fixed

--- noteline/kf/noteline_nb_op.py
+++ noteline/kf/noteline_nb_op.py
@@ -1,4 +1,4 @@
-from noteline import core
+from noteline.core import noteline_notebook, envs
 
 
 from kfp.dsl._container_op import ContainerOp
@@ -6,35 +6,47 @@ from kfp.dsl._container_op import ContainerOp
 
 class NotelineNbOp(ContainerOp):
 
-    def __init__(self,
-                 notebook_in: str,
-                 notebook_out: str = None,
-                 op_name: str = None,
-                 image: str = None,
-                 **kwargs):
-        if not notebook_in:
-          raise ValueError("notebook_in can't be empty. Current value: {}".format(
-              notebook_in))
-        if not notebook_out:
-          notebook_out = "out_{}".format(notebook_in)
-        if not image:
-          image = "gcr.io/deeplearning-platform-release/tf-cpu"
+  def __init__(self,
+               notebook_in: str,
+               notebook_out: str = None,
+               op_name: str = None,
+               image: str = None,
+               **kwargs):
+    if not notebook_in:
+      raise ValueError("notebook_in can't be empty. Current value: {}".format(
+          notebook_in))
+    if not notebook_out:
+      notebook_out = notebook_in
 
-        if image and image in kwargs:
-          raise ValueError(
-              "image should be set as input variable, and not via kwargs")
+    noteline_notebook_obj = noteline_notebook.get_noteline_notebook(notebook_in)
+    env = noteline_notebook_obj.get_env()
+    if env["type"] == envs.DOCKER_ENV_TYPE:
+      if image:
+        print("looks like both input env ({}) provided and env from"
+              " notebook({}). Using env provided explicitly".format(image,
+                                                                    str(env)))
+      else:
+        image = env["uri"]
 
-        if not op_name:
-          op_name = notebook_in
+    if not image:
+      image = "gcr.io/deeplearning-platform-release/tf-cpu"
 
-        if "command" in kwargs:
-          raise ValueError("command should NOT be passed in kwargs")
-        if "arguments" in kwargs:
-          raise ValueError("arguments should NOT be passed in kwargs")
+    if image and image in kwargs:
+      raise ValueError(
+          "image should be set as input variable, and not via kwargs")
 
-        kwargs["image"] = image
-        kwargs["command"] = ["nbexecutor"]
-        kwargs["arguments"] = ["--input-notebook", notebook_in,
-                               "--output-notebook", notebook_out]
+    if not op_name:
+      op_name = notebook_in.replace("-", "_").replace(".", "_").replace("/",
+                                                                        "_")
 
-        super().__init__(name=op_name, **kwargs)
+    if "command" in kwargs:
+      raise ValueError("command should NOT be passed in kwargs")
+    if "arguments" in kwargs:
+      raise ValueError("arguments should NOT be passed in kwargs")
+
+    kwargs["image"] = image
+    kwargs["command"] = ["nbexecutor"]
+    kwargs["arguments"] = ["--input-notebook", notebook_in,
+                           "--output-notebook", notebook_out]
+
+    super().__init__(name=op_name, **kwargs)
